8281c67bef583b6d3fa1b63c5fdec8552646fa05
amirnssr <amirnassiri4@gmail.com>
2025-02-13 18:52:24 +0100

    suppression d'un dossier doublon

--- projetsIntelliJ/tp1/src/ConstantesErreurs.java
+++
@@ -1,7 +0,0 @@
-public interface ConstantesErreurs {
-    public final String [] ERREURS_PLANNING={
-        "Réservation invalide",
-        "Planning complet",
-        "Réservation incompatible"
-    };
-}
--- projetsIntelliJ/tp1/tp1.java
+++
@@ -1,2 +0,0 @@
-public class tp1 {
-}
