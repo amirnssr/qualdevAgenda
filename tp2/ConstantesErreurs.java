307e91e30130e6c1fd88676a20946577bff6328a
amirnssr <amirnassiri4@gmail.com>
2025-02-16 22:46:35 +0100

    ajout de la doc pour l'interface constanteserreurs

--- tp2/ConstantesErreurs.java
+++ tp2/ConstantesErreurs.java
@@ -1,7 +1,16 @@
+/**
+ * L'interface ConstantesErreurs contient des constantes de messages d'erreur relatifs à la gestion du planning.
+ */
 public interface ConstantesErreurs {
-    public final String [] ERREURS_PLANNING={
-        "Réservation invalide",
-        "classes.Planning complet",
-        "Réservation incompatible"
+
+    /**
+     * Tableau des messages d'erreur possibles pour la gestion du planning.
+     *
+     * ERREURS_PLANNING contient des messages pour indiquer différents types d'erreurs dans la gestion des réservations.
+     */
+    public final String[] ERREURS_PLANNING = {
+            "Réservation invalide",       // Erreur pour une réservation invalide
+            "classes.Planning complet",    // Erreur lorsque le planning est complet
+            "Réservation incompatible"     // Erreur lorsque deux réservations sont incompatibles
     };
 }
