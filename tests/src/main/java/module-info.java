9e0baaef5d9da7a383077bbec502103431c2c835
amirnssr <amirnassiri4@gmail.com>
2025-02-16 22:31:35 +0100

    ajout de la doc pour constantescalendrier

--- tests/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tests {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tests to javafx.fxml;
-    exports com.example.tests;
-}
--- tp2/classes/ConstantesCalendrier.java
+++ tp2/classes/ConstantesCalendrier.java
@@ -1,8 +1,17 @@
 package classes;
 
+/**
+ * Interface ConstantesCalendrier
+ * Cette interface définit des constantes liées au calendrier telles que les jours de la semaine et les mois de l'année.
+ */
 public interface ConstantesCalendrier {
+    /**
+     * Tableau des jours de la semaine en français.
+     */
     final String [] JOURS_SEMAINE={"lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"};
-    final String [] MOIS={"janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"};
 
-    
+    /**
+     * Tableau des mois de l'année en français.
+     */
+    final String [] MOIS={"janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"};
 }
