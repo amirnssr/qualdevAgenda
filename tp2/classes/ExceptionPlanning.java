62ef6ed4e47d599b9e2d62fa98507b596c6c1be0
amirnssr <amirnassiri4@gmail.com>
2025-02-16 22:40:13 +0100

    ajout de la doc pour la classe exceptionplanning

--- tp2/classes/ExceptionPlanning.java
+++ tp2/classes/ExceptionPlanning.java
@@ -1,14 +1,31 @@
+/**
+ * Classe ExceptionPlanning représentant une exception spécifique pour le planning.
+ */
 package classes;
 
+/**
+ * ExceptionPlanning est une classe d'exception personnalisée qui contient un code d'erreur.
+ */
 public class ExceptionPlanning extends Exception {
+    /**
+     * Code d'erreur associé à l'exception.
+     */
     public int chCodeErreur;
-    
-    public ExceptionPlanning(int parCodeErreur){
+
+    /**
+     * Constructeur de l'exception avec un code d'erreur.
+     * @param parCodeErreur Code d'erreur de l'exception.
+     */
+    public ExceptionPlanning(int parCodeErreur) {
         super();
-        this.chCodeErreur=parCodeErreur;
+        this.chCodeErreur = parCodeErreur;
     }
 
-    public int getCodeErreur(){
-        return  chCodeErreur;
+    /**
+     * Retourne le code d'erreur de l'exception.
+     * @return Code d'erreur.
+     */
+    public int getCodeErreur() {
+        return chCodeErreur;
     }
 }
