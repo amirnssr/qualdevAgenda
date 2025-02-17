3d3368a84222597431475429d585c66a35b71cae
amirnssr <amirnassiri4@gmail.com>
2025-02-17 17:38:55 +0100

    création du fichier de test plagehorairetest

---
+++ tp2/test/PlageHoraireTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlageHoraireTest {
+
+    @Test
+    void duree() {
+    }
+
+    @Test
+    void convert() {
+    }
+}
