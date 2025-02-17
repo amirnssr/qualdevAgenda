9afaeb38abecb967e83e70b9a2503753077ac131
amirnssr <amirnassiri4@gmail.com>
2025-02-17 17:33:49 +0100

    création du fichier de test reservationtest

---
+++ tp2/test/ReservationTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void estValide() {
+    }
+}
