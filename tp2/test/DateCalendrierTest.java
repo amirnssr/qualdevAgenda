87a515f299c0ba8f78e331c5e2383bb213260e42
amirnssr <amirnassiri4@gmail.com>
2025-02-17 17:32:50 +0100

    implémentation du fichier de test datecalendriertest

--- tp2/test/DateCalendrierTest.java
+++ tp2/test/DateCalendrierTest.java
@@ -1,7 +1,23 @@
 package test;
 
+import classes.DateCalendrier;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class DateCalendrierTest {
+public class DateCalendrierTest {
+
+    @Test
+    void dateDeLaVeille() {
+        DateCalendrier date = new DateCalendrier(1, 1, 2022);
+        DateCalendrier veille = date.dateDeLaVeille();
+        assertEquals("vendredi 31 décembre", veille.toString(), "La date de la veille est incorrecte.");
+    }
+
+    @Test
+    void dateDuLendemain() {
+        DateCalendrier date = new DateCalendrier(31, 12, 2021);
+        DateCalendrier lendemain = date.dateDuLendemain();
+        assertEquals("samedi 1 janvier", lendemain.toString(), "La date du lendemain est incorrecte.");
+    }
 
 }
